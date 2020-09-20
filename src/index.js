a6ca2360811de8a2d76821342647b60ba7a59f24
221996Vivekanand <59307548+221996Vivekanand@users.noreply.github.com>
2020-09-20 12:11:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ class StartButton extends React.Component {
   render() {
     return (
       <div style={{ marginLeft: 130 }}>
-        <button className="btn btn-lg btn-success" disabled={!this.props.value} onClick={this.props.startCountDown}>Start</button>
+        <button className="btn btn-lg btn-success" disabled={!this.props.value} onClick={this.props.startCountDown}>StartCountDwon</button>
       </div>
 
     );
